60d0246e25ad7cd71747996ba254c859142d9794
lilpinpong <signal.gaby@gmail.com>
2026-04-05 13:42:48 -0400

    Enhance project directory generation and MongoDB setup with Docker checks and random password generation

--- src/engines/generator.rs
+++ src/engines/generator.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::path::Path;
 
 
-pub fn generate_project_directory(name: &str) {
+pub fn generate_project_directory(name: &str, backend: &bool) {
     let root = Path::new(name);
 
     if root.exists() {  
@@ -11,9 +11,11 @@ pub fn generate_project_directory(name: &str) {
 
     fs::create_dir(root).expect("Failed to create project directory");
     fs::create_dir(root.join("app")).expect("Failed to create app directory");
-    fs::create_dir(root.join("server")).expect("Failed to create server directory");
-    fs::write(root.join("README.md"), format!("# {}", name)).expect("Failed to create README.md");
-    fs::write(root.join(".gitignore"), ".env*\n").expect("Failed to create .gitignore");
+    if *backend {
+        fs::create_dir(root.join("server")).expect("Failed to create server directory");
+    }
+    fs::write(root.join("README.md"), format!("# {}", name)).expect("Failed to create README.md".red());
+    fs::write(root.join(".gitignore"), ".env*\n").expect("Failed to create .gitignore".red());
 
     println!(" ✅ Generated project directory '{}'", name);
 }
--- src/engines/installer.rs
+++ src/engines/installer.rs
@@ -25,8 +25,21 @@ fn express(name: &str, path: &str){
 fn mongodb(name: &str){
     let mut rng = rand::thread_rng();
     let rand_string: String = Alphanumeric.sample_string(&mut rng, 30);
+
+    println!("Checking if Docker is installed...");
+    if !is_docker_installed() {
+        eprintln!("❌ Docker is not installed. Please install Docker to use the MongoDB feature.");
+        return;
+    }
+
+    println!("🔐 Generated random password for MongoDB: {}", rand_string);
+
     run(&format!("docker run --name db-{} -d -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD={} -v mongodb_data:/data/db mongo", name, rand_string ), ".");
-    fs::write(format!("{}.env", name), format!("MONGO_URI=mongodb://admin:{}@localhost:27017", rand_string)).expect(|_| (run!("docker rm -f db-{}", name, "."), panic!("Failed to create .env file")));
+    fs::write(format!("{}.env", name), format!("MONGO_URI=mongodb://admin:{}@localhost:27017", rand_string)).expect(|_| (run!("docker rm -f db-{}", name, "."), panic!("Failed to create .env file".red())));
     println!("✅ MongoDB container 'db-{}' created with username 'admin' and password '{}'. The connection string is saved in '{}.env'", name, rand_string, name);
     println!("⚠️ Remember this is a development setup. For production, use a secure method to manage your database credentials.");
+}
+
+fn blazor(name: &str, path: &str){
+    run(&format!("dotnet new blazorserver -o {}", name), path);
 }
