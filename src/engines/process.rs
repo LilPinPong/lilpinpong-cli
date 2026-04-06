7d7d95af4d241bf43bcb7e25a56be4b7e1a3ea36
lilpinpong <signal.gaby@gmail.com>
2026-04-06 13:41:21 -0400

    Make process check functions public for external access

--- src/engines/process.rs
+++ src/engines/process.rs
@@ -1,13 +1,13 @@
 use std::process::Command;
 
-fn is_docker_installed() -> bool {
+pub fn is_docker_installed() -> bool {
     Command::new("docker")
         .arg("--version")
         .output()
         .is_ok()
 }
 
-fn is_docker_running() -> bool {
+pub fn is_docker_running() -> bool {
     Command::new("docker")
         .arg("info")
         .output()
@@ -15,14 +15,14 @@ fn is_docker_running() -> bool {
         .unwrap_or(false)
 }
 
-fn is_git_installed() -> bool {
+pub fn is_git_installed() -> bool {
     Command::new("git")
         .arg("--version")
         .output()
         .is_ok()
 }
 
-fn is_node_installed() -> bool {
+pub fn is_node_installed() -> bool {
     Command::new("node")
         .arg("--version")
         .output()
